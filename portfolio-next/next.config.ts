13190d8e2e38d05292906e266b35c261f1b3c439
ikram <89005135+EtramaDiRaizel@users.noreply.github.com>
2026-03-10 13:17:37 +0800

    Update next.config.ts

--- portfolio-next/next.config.ts
+++ portfolio-next/next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    useDeploymentId: true,
-    // Optionally, use with Server Actions
-    useDeploymentIdServerActions: true,
-  },
 };
 
 export default nextConfig;
